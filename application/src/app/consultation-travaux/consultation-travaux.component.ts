00d63b501fa363348a72edb59a2a3b353d0d0a3c
Dannick Bilodeau <bild2707@usherbrooke.ca>
2023-06-15 16:34:42 -0400

    debut page travaux

---
+++ application/src/app/consultation-travaux/consultation-travaux.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-consultation-travaux',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: `./consultation-travaux.component.html`,
+  styleUrls: ['./consultation-travaux.component.css']
+})
+export class ConsultationTravauxComponent {
+
+}
--- application/src/app/main-view/main-view.component.ts
+++ application/src/app/main-view/main-view.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursComponent } from '../cours/cours.component';
+import { ConsultationTravauxComponent } from '../consultation-travaux/consultation-travaux.component';
 import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [CommonModule, CoursComponent],
+  imports: [CommonModule, CoursComponent, ConsultationTravauxComponent],
   templateUrl: `./main-view.component.html`,
   styleUrls: ['./main-view.component.css']
 })
