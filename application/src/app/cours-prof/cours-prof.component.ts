f3cea206e7f18d48593776dc1eb46ba1dcf50b7f
Dannick Bilodeau <bild2707@usherbrooke.ca>
2023-06-29 15:34:47 -0400

    mon commit 123

---
+++ application/src/app/cours-prof/cours-prof.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cours-prof',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cours-prof.component.html',
+  styleUrls: ['./cours-prof.component.css']
+})
+export class CoursProfComponent {
+
+}
