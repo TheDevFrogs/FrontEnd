57eb692b52380bb3baf1ba75635f994d704ddaab
Dannick Bilodeau <bild2707@usherbrooke.ca>
2023-07-06 17:00:30 -0400

    correction travaux again

---
+++ application/src/app/correction-remise/correction-remise.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-correction-remise',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './correction-remise.component.html',
+  styleUrls: ['./correction-remise.component.css']
+})
+export class CorrectionRemiseComponent {
+
+}
