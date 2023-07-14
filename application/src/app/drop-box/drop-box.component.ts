6678582f5acb73ccc2fa2a4139bdaad9b4b1a93c
Simon <simsimtrudeau@gmail.com>
2023-07-13 20:53:12 -0400

    Ajout des cours dynamiques en fct de la session

--- application/src/app/drop-box/drop-box.component.ts
+++ application/src/app/drop-box/drop-box.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxFileDropEntry, NgxFileDropModule } from 'ngx-file-drop';
+import { FileSystemFileEntry, NgxFileDropEntry, NgxFileDropModule } from 'ngx-file-drop';
+import { saveAs } from 'file-saver';
+import * as JSZip from 'jszip';
+import { Observable } from 'rxjs';
+import { AuthedUserService } from '../authed-user.service';
 
 @Component({
   selector: 'app-drop-box',
@@ -10,11 +14,49 @@ import { NgxFileDropEntry, NgxFileDropModule } from 'ngx-file-drop';
   styleUrls: ['./drop-box.component.css']
 })
 export class DropBoxComponent {
+
   public files: NgxFileDropEntry[] = [];
 
-  public send(){
-    console.log("Send...");
+  public zip = new JSZip();
+
+  public userAuthed : AuthedUserService;
+
+  constructor(user : AuthedUserService){
+    this.userAuthed = user;
   }
+
+  public send(){
+    this.zipAndSend();
+  }
+
+
+  saveZip(){
+    this.zip.generateAsync({type:"blob"}).then((content) => {
+      this.userAuthed.uploadFile(content);
+    });
+  }
+
+  zipAndSend(){
+
+    var counter = 0;
+
+    for (var droppedFile of this.files) {
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+          fileEntry.file((file: File) => {
+            counter++;
+            this.zip.file(file.name, file);
+
+            if(counter == this.files.length){
+              this.saveZip();
+            }
+        });
+      } 
+    }
+
+  }
+
 
   public cancel(){
     this.files = [];
@@ -22,40 +64,6 @@ export class DropBoxComponent {
 
   public dropped(files: NgxFileDropEntry[]) {
     this.files = this.files.concat(files);
-    
-    for (const droppedFile of files) {
-
-      // Is it a file?
-      if (droppedFile.fileEntry.isFile) {
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        fileEntry.file((file: File) => {
-
-          // Here you can access the real file
-          console.log(droppedFile.relativePath, file);
-
-          /**
-          // You could upload it like this:
-          const formData = new FormData()
-          formData.append('logo', file, relativePath)
-
-          // Headers
-          const headers = new HttpHeaders({
-            'security-token': 'mytoken'
-          })
-
-          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
-          .subscribe(data => {
-            // Sanitized logo returned from backend
-          })
-          **/
-
-        });
-      } else {
-        // It was a directory (empty directories are added, otherwise only files)
-        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-        console.log(droppedFile.relativePath, fileEntry);
-      }
-    }
   }
 
   public fileOver(event){
