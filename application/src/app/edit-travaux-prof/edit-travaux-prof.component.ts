a77b5cffc23e93f8fa3d63cb9caf3dfcfbac00e7
Dannick Bilodeau <bild2707@usherbrooke.ca>
2023-07-15 14:47:15 -0400

    gossage de date cancer

--- application/src/app/edit-travaux-prof/edit-travaux-prof.component.ts
+++ application/src/app/edit-travaux-prof/edit-travaux-prof.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { PopUpComponent } from '../pop-up/pop-up.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 
 @Component({
   selector: 'app-edit-travaux-prof',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDatepickerModule, MatFormFieldModule],
   templateUrl: './edit-travaux-prof.component.html',
   styleUrls: ['./edit-travaux-prof.component.css']
 })
